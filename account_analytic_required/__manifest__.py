18ade8a4bb51f84494cc70616e14988812f1e859
BT-dherreros <daniel.herreros@braintec-group.com>
2021-12-09 16:05:13 +0100

    [t126096][MIG] Migration of account_analytic_required

--- account_analytic_required/__manifest__.py
+++ account_analytic_required/__manifest__.py
@@ -3,12 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 {
     "name": "Account Analytic Required",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Analytic Accounting",
     "license": "AGPL-3",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-analytic",
-    "depends": ["account_menu"],
+    "depends": [
+        "account",
+    ],
     "data": ["views/account_account_type.xml"],
     "installable": True,
 }
